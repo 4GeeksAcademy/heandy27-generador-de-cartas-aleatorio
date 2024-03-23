d2ffdf56478d8668daa2d0e574c9b6a4678e13af
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-03-23 02:28:43 +0000

    [Actualización de index,style y app.js] [Creación de carta en el index] [Creación de estilos para la carta] [Creación de lógica en javascript]

--- src/app.js
+++ src/app.js
@@ -8,4 +8,51 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let numberCards = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let symbolsCard = ["♦", "♥", "♠", "♣"];
+  console.log({ symbolsCard });
+
+  function elementRandomGenerator(elements) {
+    let randomNumber = Math.floor(Math.random() * (elements.length - 1));
+    return elements[randomNumber];
+  }
+
+  let resultCardGenerator = elementRandomGenerator(numberCards);
+  let resultSymbolGenerator = elementRandomGenerator(symbolsCard);
+
+  let numberElementCard = document.querySelector(".number");
+  numberElementCard.innerHTML = resultCardGenerator;
+
+  let symbolTopElementCard = document.querySelector(".symbol-top");
+  symbolTopElementCard.innerHTML = resultSymbolGenerator;
+
+  let symbolBottomElementCard = document.querySelector(".symbol-bottom");
+  symbolBottomElementCard.innerHTML = resultSymbolGenerator;
+
+  console.dir(symbolBottomElementCard);
+
+  function getSymbolColor(elementColor) {
+    if (elementColor.innerHTML == "♥" || elementColor.innerHTML == "♦") {
+      elementColor.style.color = "red";
+    } else {
+      elementColor.style.color = "black";
+    }
+  }
+
+  let symbolTopColor = getSymbolColor(symbolTopElementCard);
+  let symbolBottom = getSymbolColor(symbolBottomElementCard);
 };
